ad1e19a704c37324444a46f702484b683fda8daa
Sumit Soni <sumit311shk@gmail.com>
2026-02-09 19:15:23 +0530

    fix react router netlify

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://aman-ftx.netlify.app"
+  "https://aman-ftx.netlify.app",
+  "https://ftxmedia.netlify.app",
 ];
 
 app.use(cors({
